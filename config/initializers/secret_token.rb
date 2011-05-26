48d7d1453ad34ae0f8f702dd1c11e9fa88238386
dong41 <tony.seing@gmail.com>
2011-05-26 01:04:02 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chatapp::Application.config.secret_token = 'db3f3fabaf73d86c2dc54738010151464572697f7e1bad5b04753fbb532381ac71c87c860a0680c013679ea25805499532295bf9dc7b162a2ec1fb64831c68e6'
