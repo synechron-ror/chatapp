3f892f2f62554d751fadda5c450e251c669376f9
Tony Seing <tony.seing@gmail.com>
2011-06-02 19:05:01 -0400

    messages working in one direction

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
  $(function() {  
-   var faye = new Faye.Client('http://localhost:8000/faye');  
+  var faye = new Faye.Client('http://localhost:8000/faye');  
+   // alert('subscribing!')  
    faye.subscribe('/messages/new', function (data) {  
-     alert(data);  
+     eval(data);  
    });  
- });  
+});  
