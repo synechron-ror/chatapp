a7d180c0e6f648d0979f787d69558d47a3a0d8f1
Tony Seing <tony.seing@gmail.com>
2011-06-02 18:55:46 -0400

    most config errors gone.  message posting to faye

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,12 @@
 class MessagesController < ApplicationController
- 
+   
    def create  
      @message = Message.create!(params[:message])  
    end  
   
   def index
-    @messages = Message.all  
+    @messages = Message.all
+    render 'messages/index.html.erb'
   end
 
 end
